cef18508b78cc073de1e0a9f8ebd44dcbb6d8503
Pascal Allau <pascal.allau@zohomail.eu>
2023-02-11 16:11:13 +0100

    feat:create ErrorPage component and add error page to the router

---
+++ src/pages/error-page/ErrorPage.tsx
@@ -0,0 +1,26 @@
+import Footer from 'components/footer/Footer';
+import Header from 'components/header/Header';
+import { Link, useRouteError } from 'react-router-dom';
+import styles from './ErrorPage.module.scss';
+
+interface IErrorPage {
+  status: number;
+  statusText: string;
+  internal: boolean;
+  data: string;
+}
+function ErrorPage() {
+  const error = useRouteError() as IErrorPage;
+  return (
+    <>
+      <Header />
+      <div className={styles.div}>
+        <h1>{error.status}</h1>
+        <p>Oups! La page que vous demandez n&apos;existe pas.</p>
+        <Link to="/">Retourner sur la page d&apos;accueil</Link>
+      </div>
+      <Footer />
+    </>
+  );
+}
+export default ErrorPage;
--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,12 +1,14 @@
 import { createBrowserRouter } from 'react-router-dom';
 import About from 'pages/about/About';
 import Home from 'pages/home/Home';
+import ErrorPage from 'pages/error-page/ErrorPage';
 import Root from './Root';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Root />,
+    errorElement: <ErrorPage />,
     children: [
       {
         index: true,
